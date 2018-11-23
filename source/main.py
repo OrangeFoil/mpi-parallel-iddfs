fce8d44e0f0a18ed7486a7a2cbda876bd6ac9118
Marcus Legendre <marcus.legendre@gmail.com>
2018-11-23 17:09:39 +0100

    Increase n and distribute tasks until a solution is found

--- source/main.py
+++ source/main.py
@@ -13,27 +13,39 @@ def do_some_task(n: int):
     else:
         return (False, n, None)
 
-# distribute tasks
+# coordination
+solution_found = False
 if rank == 0:
-    n = [i + 40 for i in range(size)]
-else:
-    n = None
-n = MPI.COMM_WORLD.scatter(n, root=0)
+    batch = 0
 
-# compute results
-if rank == 0:
-    data = None
-else:
-    data = do_some_task(n)
+while not solution_found:
+    # distribute tasks
+    if rank == 0:
+        num_worker_nodes = size - 1
+        n = [batch * num_worker_nodes + i for i in range(size)]
+    else:
+        n = None
+    n = MPI.COMM_WORLD.scatter(n, root=0)
 
-# collect results
-data = MPI.COMM_WORLD.gather(data, root=0)
-if rank == 0:
-    for i in range(1, size):
-        if data[i][0]:
-            print("Node", i, "found a solution for n =",
-                  data[i][1], "=>", data[i][2])
-        else:
-            print("Node", i, "did not find a solution for n =", data[i][1])
-else:
-    assert data is None
+    # compute results
+    if rank == 0:
+        data = None
+    else:
+        data = do_some_task(n)
+
+    # collect results
+    data = MPI.COMM_WORLD.gather(data, root=0)
+    if rank == 0:
+        for i in range(1, size):
+            if data[i][0]:
+                print("Node", i, "found a solution for n =",
+                      data[i][1], "=>", data[i][2])
+                solution_found = True
+            else:
+                print("Node", i, "did not find a solution for n =", data[i][1])
+
+    # distribute solution_found variable
+    if rank == 0:
+        solution_found = [solution_found for _ in range(size)]
+        batch += 1
+    solution_found = MPI.COMM_WORLD.scatter(solution_found, root=0)
