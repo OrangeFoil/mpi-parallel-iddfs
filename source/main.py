018c1c850c88379f456bd37c415a09c0adb3de2d
Marcus Legendre <marcus.legendre@gmail.com>
2018-11-23 06:35:11 +0100

    Proof of concept for distributing tasks, collect and evaluate results

--- source/main.py
+++ source/main.py
@@ -6,10 +6,30 @@ size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 name = MPI.Get_processor_name()
 
-#data = (rank + 1)**2
-data = "I am process {} of {} on {}".format(rank, size, name)
+
+def do_some_task(n: int):
+    if n == 42:
+        return True, "I am #42"
+    else:
+        return False, None
+
+# distribute tasks
+if rank == 0:
+    n = [i + 40 for i in range(size)]
+else:
+    n = None
+n = MPI.COMM_WORLD.scatter(n, root=0)
+
+# compute results
+if rank == 0:
+    data = None
+else:
+    data = do_some_task(n)
+
+# collect results
 data = MPI.COMM_WORLD.gather(data, root=0)
 if rank == 0:
-    print("Got data", data)
+    for i in range(size):
+        print("Node", i, "reported", data[i])
 else:
     assert data is None
